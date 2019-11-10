b267cae7fa9715a3355d5bf1df998d0bc4c0141b
Siva Girish <sivagirish81@gmail.com>
2019-11-10 11:06:32 +0530

    Modules aand Requirements

--- Display_Drivers.c
+++ Display_Drivers.c
@@ -1,10 +1,15 @@
-//DDisplay the device driver / module statistics currently active in the Operating system. 
+//Display the device driver / module statistics currently active in the Operating system. 
 //Hint: lsmod, modinfo
 
+//Modules Required
 #include<stdio.h> 
 #include<stdlib.h> 
 #include<unistd.h>
+#include<string.h>
 
+//First redirect the lsmod info to a file
+//Take the contents of the file and pass it aas input to the program.
+//Based on whether the driver is used or not display the driver statistics.
 int main()
 {
 
