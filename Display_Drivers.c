ea3616d6fabeb09ae422f62aa8a0a79d54d4eecf
Siva Girish <sivagirish81@gmail.com>
2019-11-10 10:56:05 +0530

    Initiating Code

---
+++ Display_Drivers.c
@@ -0,0 +1,11 @@
+//DDisplay the device driver / module statistics currently active in the Operating system. 
+//Hint: lsmod, modinfo
+
+#include<stdio.h> 
+#include<stdlib.h> 
+#include<unistd.h>
+
+int main()
+{
+
+}
