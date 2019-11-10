ba5286aadec0290f4704e2308e62b78da0cb905d
Parshva <pashwab255225@gmail.com>
2019-11-10 11:43:35 +0530

    outputting command output into file

--- Display_Drivers.c
+++ Display_Drivers.c
@@ -6,26 +6,29 @@
 #include<stdlib.h> 
 #include<unistd.h>
 #include<string.h>
-
-
+#include<fcntl.h>
+#include<sys/wait.h>
 
 //First redirect the lsmod info to a file
 //Take the contents of the file and pass it aas input to the program.
 //Based on whether the driver is used or not display the driver statistics.
 int main()
 {
-    int fd = open("lsmod_file.txt","O_RDWR");
+    
 
     int id = fork();
 
     if(id == 0){
-
+    int fd = open("lsmod_file.txt",O_CREAT | O_RDWR);
+    dup2(fd,1);    
     char* lsmod_output[2]={"lsmod",NULL};
     execvp(lsmod_output[0],lsmod_output);
-
+    close(fd);
     }
-    char* lsmod_output[2]={"lsmod",NULL};
-    execvp(lsmod_output[0],lsmod_output);
+    else{
+        wait(0);
+    }
+    
 
     //Fd for std output 1
 
